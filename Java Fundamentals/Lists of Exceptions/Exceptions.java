2682cc5849d737bfee3108b78147cb51dbcb58c7
Mohamamd Yahya <mohammad1.yahya2019@gmail.com>
2023-12-27 21:05:38 +0200

    Exceptions

---
+++ Java Fundamentals/Lists of Exceptions/Exceptions.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Exceptions {
+
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        for (int i = 0; i < myList.size(); i++) {
+            try {
+
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println("Cast is invalid" + castedValue);
+
+            } catch (ClassCastException e) {
+                System.out.println("error" + e);
+                System.out.println("Value that triggered the error: " + myList.get(i));
+            }
+
+        }
+        System.out.println("The program should continue after printing this to the console");
+    }
+}
