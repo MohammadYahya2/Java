d57bdc3e2b066aa2ad4430bb97f0322d35235b1e
Mohamamd Yahya <mohammad1.yahya2019@gmail.com>
2024-01-03 12:02:37 +0200

    Hopper's Receipt/

---
+++ spring/Hopper's Receipt/HopperReceipt/src/test/java/com/example/hopperreceipt/HopperReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hopperreceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopperReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
